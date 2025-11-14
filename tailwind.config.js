056e201c9313a53334fcfbd4a3caab10b3509564
Julian <oconjuli.178@gmail.com>
2025-11-14 14:36:03 -0600

    index hecho

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        azul: "#007BFF",
+        negro: "#0B0B0B",
+      },
+    },
+  },
+  plugins: [],
+}
